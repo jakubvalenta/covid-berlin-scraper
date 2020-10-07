fbd77a9acb909fb4e1b303d5a0a43d32825cb757
Jakub Valenta <jakub@jakubvalenta.cz>
2020-10-07 13:03:54 +0200

    Version 0.8.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from covid_berlin_scraper import __title__
 
 setup(
     name='covid-berlin-scraper',
-    version='0.7.2',
+    version='0.8.0',
     description=__title__,
     long_description=(Path(__file__).parent / 'README.md').read_text(),
     url='https://www.github.com/jakubvalenta/covid-berlin-scraper',
